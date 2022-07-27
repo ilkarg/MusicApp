acf39d8c65d82eeb9b0e5dd2bb603952e4a4a57c
Ilya Khrebtov <zkhrebtovilya@gmail.com>
2022-07-27 20:56:54 +0600

    Создан макет интерфейса | Разработаны основные функции - проигрывания, паузы и перезапуска проигрывания музыки

---
+++ MusicApp/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace MusicApp
+{
+    public partial class MainWindow : Window
+    {
+        private Song? _song;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PauseSoundButtonClick(object sender, RoutedEventArgs e) => 
+            _song?.PausePlay();
+
+        private void PlaySoundButtonClick(object sender, RoutedEventArgs e) => 
+            _song?.StartPlay();
+
+        private void StopSoundButtonClick(object sender, RoutedEventArgs e) => 
+            _song?.StopPlay();
+
+        private void AddSoundButtonClick(object sender, RoutedEventArgs e) =>
+            _song = new Song("Бара Бара Бере Бере", new Uri("./Music/test.mp3", UriKind.RelativeOrAbsolute));
+    }
+}
---
+++ MusicApp/Song.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MusicApp;
+
+public class Song
+{
+    public string? Name { get; set; }
+    public Uri Path { get; set; }
+    public bool IsPlayed { get; set; }
+    private readonly MediaPlayer _mediaPlayer;
+
+    public Song(string? name, Uri path)
+    {
+        Name = name;
+        Path = path;
+        IsPlayed = false;
+        _mediaPlayer = new MediaPlayer();
+        _mediaPlayer.Open(Path);
+        _mediaPlayer.MediaFailed += (s, e) => MessageBox.Show(e.ErrorException.Message, "Ошибка");
+    }
+
+    public bool StartPlay()
+    {
+        try
+        {
+            _mediaPlayer.Play();
+            IsPlayed = true;
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show(exception.Message, "Ошибка");
+            return false;
+        }
+        
+        return true;
+    }
+
+    public bool PausePlay()
+    {
+        try
+        {
+            _mediaPlayer.Pause();
+            IsPlayed = false;
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show(exception.Message, "Ошибка");
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool StopPlay()
+    {
+        try
+        {
+            _mediaPlayer.Stop();
+            IsPlayed = false;
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show(exception.Message, "Ошибка");
+            return false;
+        }
+
+        return true;
+    }
+}
